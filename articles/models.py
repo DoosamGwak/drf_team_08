79d78b733fd93e7c5fa7f020f3a36b905c9a8468
kwang1215 <xman1215@naver.com>
2024-09-12 16:21:51 +0900

    feat: articles 기능구현
        1. articles models.py 생성
                /Article 모델 생성
        2. articles views.py
                /기사생성/기사리스트조회
        3. articles serializers.py
                /ArticleSerializer 생성
        4. articles admin.py
                /Article 모델 등록

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from blame_news import settings
 
-# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    # category = models.ForeignKey()
+    # reporter = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.TextField()
+    image = models.ImageField(upload_to="images/")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    hits = models.IntegerField(blank=True, default=0)
+
+    def __str__(self):
+        return self.title
--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Article
+from .serializers import ArticleSerializer
 
-# Create your views here.
+
+class ArticleListAPIView(APIView):
+    def post(self, request):
+        serializer = ArticleSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=201)
+    
+    def get(self, request):
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True)
+        return Response(serializer.data)
