fa34651f43074a1617ecccb05c623335f710109a
Jannys <slalstkd45@gmail.com>
2024-09-12 16:13:31 +0900

    feat : 회원가입 다 한듯?
    1. serializers.py 생성 > 'id','username','nickname','email','gender'
       추가
    2. validators.py 생성 > username,password,email 조건 걸어줌
    3. views.py 생성 > post - 회원가입 로직 추가
    4. (의미없음) accounts/templates 폴더 생성 > 나중에 쓸려고?

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate
+from .models import User
+from .validators import validate_user_data
+from .serializers import UserSerializer
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.tokens import RefreshToken
+
+
 
 # Create your views here.
+class UserCreateView(APIView):
+    def post(self, request):
+        
+        # permission_classes = [AllowAny]
+        
+        # rlt_message = validate_user_data(request.data)
+        
+        # if rlt_message is not None:
+        #     return Response({"messgae":rlt_message}, status=400)
+        
+        user = User.objects.create_user(**request.data)
+        
+        serializer = UserSerializer(user)
+        
+        return Response(serializer.data)
