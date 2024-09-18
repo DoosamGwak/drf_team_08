ecb6e9fb9a52d961078a6d374bb127f1e098c4be
Seungju_Yi <yi77371463@gmail.com>
2024-09-18 21:42:49 +0900

    fix: article list 조회 및 생성 로직 수정

--- articles/views.py
+++ articles/views.py
@@ -10,7 +10,7 @@ from rest_framework.permissions import (
 )
 from rest_framework.generics import ListCreateAPIView
 from django.shortcuts import get_object_or_404
-from .models import Article,Comment,Image,Category
+from .models import Article, Comment, Image, Category
 from .serializers import (
     ArticleListSerializer,
     ArticleCreateSerializer,
@@ -29,35 +29,53 @@ class ArticleListAPIView(ListCreateAPIView):
     serializer_class = ArticleListSerializer
     permission_classes = [ReporterOrReadOnly]
 
-    def get(self, request):
-        self.permission_classes = [AllowAny] 
+    def get(self, request, *args, **kwargs):
         user = request.user
-
         # 사용자가 회원인 경우
         if user.is_authenticated:
             blinded_reporters = user.blinding.all()
-            articles = Article.objects.exclude(reporter__in=blinded_reporters)  # 블라인드한 기자의 기사 제외
-        else:
-            # 비회원에게는 모든 기사 제공
-            articles = Article.objects.all()
+            self.queryset = Article.objects.exclude(
+                reporter__in=blinded_reporters
+            )  # 블라인드한 기자의 기사 제외
+        return super().get(request, *args, **kwargs)
 
-        paginator = self.pagination_class()
-        paginated_articles = paginator.paginate_queryset(articles, request)
-        serializer = ArticleListSerializer(paginated_articles, many=True)
-        return paginator.get_paginated_response(serializer.data)
+    # def get(self, request):
+    #     self.permission_classes = [AllowAny]
+    #     user = request.user
 
+    #     # 사용자가 회원인 경우
+    #     if user.is_authenticated:
+    #         blinded_reporters = user.blinding.all()
+    #         articles = Article.objects.exclude(
+    #             reporter__in=blinded_reporters
+    #         )  # 블라인드한 기자의 기사 제외
+    #     else:
+    #         # 비회원에게는 모든 기사 제공
+    #         articles = Article.objects.all()
+
+    #     paginator = self.pagination_class()
+    #     paginated_articles = paginator.paginate_queryset(articles, request)
+    #     serializer = ArticleListSerializer(paginated_articles, many=True)
+    #     return paginator.get_paginated_response(serializer.data)
 
     def post(self, request, *args, **kwargs):
         self.serializer_class = ArticleCreateSerializer
-        return super().post(request, *args, **kwargs)
-
-    def perform_create(self, serializer):
-        images = self.request.FILES.getlist("images")
+        serializer = self.serializer_class
+        images = request.FILES.getlist("images")
         if not images:  # 이미지 key error 처리
             return Response({"ERROR": "Image file is required."}, status=400)
-        article = serializer.save(reporter=self.request.user)
+        article = serializer.save(reporter=request.user)
         for image in images:
             Image.objects.create(article=article, image_url=image)
+        return super().post(request, *args, **kwargs)
+
+    # def perform_create(self, serializer):
+    #     images = self.request.FILES.getlist("images")
+    #     if not images:  # 이미지 key error 처리
+    #         return Response({"ERROR": "Image file is required."}, status=400)
+    #     article = serializer.save(reporter=self.request.user)
+    #     for image in images:
+    #         Image.objects.create(article=article, image_url=image)
 
 
 # 기사 세부 조회 수정 및 삭제
@@ -138,52 +156,56 @@ class CommentEditAPIView(APIView):
     def delete(self, request, comment_pk):
         comment = self.get_object(comment_pk)
         if Comment.DoesNotExist:
-            return Response({"error": "이 댓글을 찾을 수 없습니다."}, status= 404)
-        comment = Comment.objects.get(pk=comment_pk,is_deleted=False)
+            return Response({"error": "이 댓글을 찾을 수 없습니다."}, status=404)
+        comment = Comment.objects.get(pk=comment_pk, is_deleted=False)
         comment.delete()
-        return Response({"detail": "댓글이 삭제되었습니다."},status=204)
-    
+        return Response({"detail": "댓글이 삭제되었습니다."}, status=204)
+
 
 # 카테고리 생성 및  목록 조회
 class CategoryAPIView(APIView):
-    
-    permission_classes = [IsAdminUser] # 관리자만 접근 가능
+
+    permission_classes = [IsAdminUser]  # 관리자만 접근 가능
+
     def post(self, request):
         serializer = CategorySerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=201)
         return Response({"Error": "이미 생성된 카테고리 입니다."}, status=400)
-    
-    permission_classes = [IsAuthenticated] #회원만 접근 가능
+
+    permission_classes = [IsAuthenticated]  # 회원만 접근 가능
+
     def get(self, request):
         category = Category.objects.all()
         serializer = CategorySerializer(category, many=True)
         return Response(serializer.data, status=200)
-    
+
 
 # 카테고리 수정 및  삭제
-class CategoryEditAPIView(APIView): 
+class CategoryEditAPIView(APIView):
 
-    permission_classes = [IsAdminUser] # 관리자만 접근 가능
+    permission_classes = [IsAdminUser]  # 관리자만 접근 가능
+
     def get_object(self, pk):
         return get_object_or_404(Category, pk=pk)
-    
+
     def put(self, request, category_pk):
         category = self.get_object(category_pk)
         serializer = CategorySerializer(category, data=request.data, partial=True)
         if serializer.is_valid(raise_exception=True):
             serializer.save()
             return Response(serializer.data, status=200)
-    
+
     def delete(self, request, category_pk):
         category = self.get_object(category_pk)
         category.delete()
         return Response({"detail": "카테고리가 삭제되었습니다."}, status=204)
-    
+
+
 # 싫어요 기능
 class HateAPIView(APIView):
-    permission_classes = [IsAuthenticated] #회원만 접근 가능
+    permission_classes = [IsAuthenticated]  # 회원만 접근 가능
 
     def post(self, request, pk):
         article = get_object_or_404(Article, pk=pk)
@@ -191,6 +213,6 @@ class HateAPIView(APIView):
         if request.user in article.hate.all():
             article.hate.remove(request.user)
             return Response("싫어요! 취소했습니다.", status=200)
-        
+
         article.hate.add(request.user)
         return Response("싫어요! 했습니다.", status=200)
